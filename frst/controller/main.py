f3fd8983b2691617aabf399d279526950e271dc5
shaifurcodes <shaifur.at.buet@gmail.com>
2023-02-24 02:04:30 +0600

    lora format integrated

--- frst/controller/main.py
+++ frst/controller/main.py
@@ -11,28 +11,28 @@ class FRSTController(object):
     def __init__(self):
         self.LORA_PORT = '/dev/ttyUSB0'
         self.lora_node = Lora(serial_port=self.LORA_PORT)
-        self.node_list = [2, 4]
-        self.LORA_RESP_WAIT_TIME_SEC = 0.4
+        self.node_list = [2, 3, 4]
+        self.LORA_RESP_WAIT_TIME_SEC = 0.5
         return
 
-    def run_simple_raning(self):
-        msg_counter = 0
-        while True:
-            try:
-                self.lora_node.lora_send("cb 2 4") #TODO: ENSURE FORMAT cb
-                time.sleep(5) #TODO: DECREASE SLEEEP TIME
-                lora_msg = self.lora_node.lora_receive()
-                msg_counter += 1
-                print(str(msg_counter)+" lora_msg:"+str(lora_msg))
-            except Exception as ex:
-                print(ex)
-                self.lora_node.close_serial_port()
+    # def run_simple_raning(self):
+    #     msg_counter = 0
+    #     while True:
+    #         try:
+    #             self.lora_node.lora_send("cb 2 4") #TODO: ENSURE FORMAT cb
+    #             time.sleep(5) #TODO: DECREASE SLEEEP TIME
+    #             lora_msg = self.lora_node.lora_receive()
+    #             msg_counter += 1
+    #             print(str(msg_counter)+" lora_msg:"+str(lora_msg))
+    #         except Exception as ex:
+    #             print(ex)
+    #             self.lora_node.close_serial_port()
 
     def run_ranging(self):
         while True:
             try:
                 for n1 in self.node_list:
-                    lora_msg=str(n1)
+                    lora_msg='cb '+str(n1)
                     for n2 in self.node_list:
                         if n1!=n2:
                             lora_msg =lora_msg+' '+str(n2)
@@ -40,10 +40,13 @@ class FRSTController(object):
                     finishing_ts = time.time()+self.LORA_RESP_WAIT_TIME_SEC
                     while time.time() < finishing_ts:
                         lora_msg = self.lora_node.lora_receive()
-                        lora_msg = ''.join(letter for letter in lora_msg if letter.isalnum() or letter in [',', ' ']) #TODO: recheck if characters are dropped
-                        if lora_msg:
-                            print("src-node: "+str(n1)+"=========")
-                            print(lora_msg)
+                        if not lora_msg:
+                            continue
+                        lora_msg = ''.join(letter for letter in lora_msg if letter.isalnum() or letter in [' ']) #TODO: recheck if characters are dropped
+                        if 'bc' in lora_msg:
+                            range_vals = lora_msg.split('bc')[-1].strip()
+                            print("ranging node:"+str(n1)+"==============")
+                            print("\trange values: (node-id range-in-cm):"+range_vals)
                             break
             except Exception as ex:
                 print(ex)
@@ -52,4 +55,4 @@ class FRSTController(object):
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, sigint_handler)
     frst_controller = FRSTController()
-    frst_controller.run_simple_raning()
+    frst_controller.run_ranging()
