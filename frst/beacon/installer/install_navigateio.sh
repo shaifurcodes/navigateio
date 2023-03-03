2535aa5fab991449e925382a54e989dd61c4042c
shaifurcodes <shaifur.at.buet@gmail.com>
2023-03-03 18:15:45 +0600

    installer debugged

--- frst/beacon/installer/install_navigateio.sh
+++ frst/beacon/installer/install_navigateio.sh
@@ -2,6 +2,8 @@
 rm -rf /usr/local/etc/navigateio && \
 mkdir -p /usr/local/etc/navigateio && \
 cp /home/pi/software/navigateio/frst/beacon/main.py /usr/local/etc/navigateio/main.py && \
+cp /home/pi/software/navigateio/frst/beacon/lora_sx126x.py /usr/local/etc/navigateio/lora_sx126x.py && \
+cp /home/pi/software/navigateio/frst/beacon/pressure_sensor_lps22hb.py /usr/local/etc/navigateio/pressure_sensor_lps22hb.py && \
 chmod +x /usr/local/etc/navigateio/main.py && \
 cp /home/pi/software/navigateio/frst/beacon/installer/navigateio.service /etc/systemd/system/navigateio.service && \
 systemctl enable navigateio.service && \
